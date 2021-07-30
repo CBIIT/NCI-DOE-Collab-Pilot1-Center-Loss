025d281ba9603463423c49a31ab19c36eeb2e064
George Zaki <george.zaki@nih.gov>
2021-07-30 15:46:37 -0400

    added data download utilities

---
+++ src/utils/download_data.py
@@ -0,0 +1,13 @@
+import os
+from file_utils import get_file
+
+response_collection_path="https://modac.cancer.gov/api/v2/dataObject/NCI_DOE_Archive/JDACS4C/JDACS4C_Pilot_1/cancer_drug_response_prediction_dataset"
+combined_cl = "combined_cl_metadata"
+cl_url = response_collection_path  + "/" + combined_cl 
+
+rnaseq = "combined_rnaseq_data"
+rnaseq_url = response_collection_path + "/" + rnaseq
+data_dest = os.path.join("data", "ftp-data")
+
+get_file(combined_cl , cl_url, datadir=data_dest)
+get_file(rnaseq, rnaseq_url, datadir=data_dest)
